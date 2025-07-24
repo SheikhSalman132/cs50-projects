a971d4a05d6a77a1d485cc6c785887beb845d597
SALMAN AHMAD SHEIKH <143435173+SheikhSalman132@users.noreply.github.com>
2025-07-24 11:48:57 +0500

    Update agree.c

--- C/agree.c
+++ C/agree.c
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-    char a = get_char("Have you ever been in a relationship? (y/n)\n");
+    char a = get_char("Have you ever read a book ? (y/n)\n");
 
     if (a == 'y' || a == 'Y')
     {
-        int b = get_int("How many people have you dated? ");
+        int b = get_int("How many books have you read? ");
         
         if (b == 0)
         {
-            printf("Looks like you haven't dated anyone yet. You'll find your match soon.\n");
+            printf("Looks like you haven't read any books yet. You'll find your book soon.\n");
         }
         else
         {
@@ -20,15 +20,15 @@ int main(void)
     }
     else 
     {
-        int c = get_int("How much would you be willing to pay for a date? ");
+        int c = get_int("How much would you be willing to pay for a book? ");
         
         if (c == 0 || c < 0)
         {
-            printf("Looks like you're not interested in spending money on dates.\n");
+            printf("Looks like you're not interested in spending money on books.\n");
         }
         else
         {
-            printf("With that budget, you might find someone special! Best of luck!\n");
+            printf("With that budget, you might find some thing special! Best of luck!\n");
         }
     }
 
