274e1be2a95b3386b0141120d9b5225dd548382e
SALMAN AHMAD SHEIKH <143435173+SheikhSalman132@users.noreply.github.com>
2025-07-24 11:51:14 +0500

    Update greet.c

--- C/greet/greet.c
+++ C/greet/greet.c
@@ -8,7 +8,7 @@ int main (int argc, char*argv[]){
         printf("how are you %s /n", argv[1]);
  }
     else{
-                printf("(fuck you did not give your name!!!???   )");
+                printf("(you did not give your name!!!???   )");
                     return 1;
 
     }
